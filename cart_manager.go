d43fd3d14669d1efd930d8b9d2e95b4f59dc027f
El-PowerPtr <moiseselpower@gmail.com>
2025-11-04 12:49:38 -0500

    feat(cart): CartManager service type created

---
+++ cart_manager.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+    "context"
+)
+
+type CartManager interface {
+    GetCart(id uint64, ctx context.Context) (*Cart, error)
+    AddCart(cart Cart, ctx context.Context) error
+    RemoveCart(uint64, context.Context) (*Cart, error)
+    AddItemInCart(uint64, uint64, int, context.Context) error
+    RemoveItemInCart(uint64, uint64, int, context.Context) error
+    FulkyRemoveItemFromCart(uint64, uint64, context.Context) error
+    ResetCart(uint64, context.Context) (*Cart, error)
+}
